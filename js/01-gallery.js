23bc2c1ee7c7eac535a9a46ea38b0aaeadea3c5a
PetroTkach <petro.tka4444@gmail.com>
2022-02-10 20:38:51 +0200

    task-01 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+// console.log("galleryContainer", galleryContainer);
+const cardsGallery = createGalleryItems(galleryItems);
+// console.log(cardsGallery);
+galleryContainer.insertAdjacentHTML("beforeend", cardsGallery);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  const cardUrl = evt.target.dataset.source;
+  // console.log("cardUrl", cardUrl);
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+}
