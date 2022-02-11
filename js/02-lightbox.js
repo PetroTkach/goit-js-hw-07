08e31b29cb4b8e7b9de4324a06d7765b70a6ee1f
PetroTkach <petro.tka4444@gmail.com>
2022-02-11 23:42:20 +0200

    task-02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+// console.log("galleryContainer", galleryContainer);
+const cardsGallery = createGalleryItems(galleryItems);
+// console.log(cardsGallery);
+galleryContainer.insertAdjacentHTML("beforeend", cardsGallery);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+
+    `;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
+console.log(lightbox);
